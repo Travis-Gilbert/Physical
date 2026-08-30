91208ca24420c998882fa8265e9e100d36fd44a9
Travis Gilbert <1travisgilbert@gmail.com>
2026-08-29 22:59:25 -0400

    Add cli entrypoint

---
+++ crates/physical-cli/src/main.rs
@@ -0,0 +1,8 @@
+//! Operator tooling. Inspect bays, list bindings, explain why a device is
+//! idle.
+
+#![forbid(unsafe_code)]
+
+fn main() {
+    println!("physical {}", env!("CARGO_PKG_VERSION"));
+}
